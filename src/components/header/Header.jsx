f9c5e0b26423e7e581bda354247c8fbc5269211c
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-18 12:45:33 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Joseph</h1>
+        <h5 className="text-light">Front End Developer</h5>
+        <CTA />
 
-export default Header
+        <div className="me">
+          <img src={ME}  alt=""me/>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
